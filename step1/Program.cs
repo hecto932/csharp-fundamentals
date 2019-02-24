a2d2c80249818e69201acdd9d48eb2297f5aaf27
Hector Flores <hecto932@gmail.com>
2019-02-24 17:08:16 -0600

    Different ways to print the elements of array

--- step1/Program.cs
+++ step1/Program.cs
@@ -3,37 +3,81 @@ using CoreSchool.Entities;
 
 namespace step1
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            var school = new School(
-                "Platzi Academy", 
-                2012, 
-                TypeSchool.Preschool,
-                city:"Bogota"
-            );
+      var school = new School(
+          "Platzi Academy",
+          2012,
+          TypeSchool.Preschool,
+          city: "Bogota"
+      );
 
-            var course1 = new Course() 
-            {
-                Name="101"
-            };
+      var arrayCourses = new Course[3];
 
-            var course2 = new Course() 
-            {
-                Name="201"
-            };
+      arrayCourses[0] = new Course()
+      {
+        Name = "101"
+      };
 
-            var course3 = new Course() 
-            {
-                Name="301"
-            };
+      arrayCourses[1] = new Course()
+      {
+        Name = "201"
+      };
 
-            Console.WriteLine(school);
-            System.Console.WriteLine("=================================");
-            Console.WriteLine(course1);
-            Console.WriteLine(course2);
-            Console.WriteLine(course3);
-        }
+      arrayCourses[2] = new Course()
+      {
+        Name = "301"
+      };
+
+      Console.WriteLine(school);
+      System.Console.WriteLine("=================================");
+      WhilePrintCourses(arrayCourses);
+      Console.WriteLine();
+      DoWhilePrintCourses(arrayCourses);
+      Console.WriteLine();
+      ForPrintCourses(arrayCourses);
+      Console.WriteLine();
+      ForEachPrintCourses(arrayCourses);
+
     }
+
+    private static void WhilePrintCourses(Course[] arrayCourses)
+    {
+      int i = 0;
+      while (i < arrayCourses.Length)
+      {
+        Console.WriteLine(arrayCourses[i]);
+        i++;
+      }
+    }
+
+    private static void DoWhilePrintCourses(Course[] arrayCourses)
+    {
+      int i = 0;
+
+      do
+      {
+        Console.WriteLine(arrayCourses[i]);
+        i++;
+      } while (i < arrayCourses.Length);
+    }
+
+    private static void ForPrintCourses(Course[] arrayCourses)
+    {
+      for (int i = 0; i < arrayCourses.Length; i++)
+      {
+        Console.WriteLine(arrayCourses[i]);
+      }
+    }
+
+    private static void ForEachPrintCourses(Course[] arrayCourses)
+    {
+      foreach (var course in arrayCourses)
+      {
+          Console.WriteLine(course);
+      }
+    }
+  }
 }
